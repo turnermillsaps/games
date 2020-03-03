30120a70167c52216693d459f16bb214da66f8ec
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-03-03 01:27:55 -0500

    Added basic variables and functions for core of nim

--- nim/main.js
+++ nim/main.js
@@ -1,8 +1,48 @@
+/*
+    Nim Abstraction
+    - 2 Players
+    - 16 pieces
+    - each player can choose up to 3 pieces
+    - player with 1 left loses
+
+    What I need
+    - variable to track player's turn
+    - function that subtracts from total amount of pieces
+    - function that updates the colors of the pieces
+        - created the pebble-yellow class for the update
+    - function that checks to see if only 1 piece is left and declares winner
+*/
+
 var content = document.getElementById('content');
+var buttonTake = document.getElementById('take');
+var inputValue = document.getElementById('takeInput').value;
+var playerOne = true;
+var totalPieces = 16;
 
-var nim = "Replace this with your own abstraction of Nim"
+// function to subtract from the total amount of pieces
+function subtractPieces(count) {
+    totalPieces -= count;
+}
 
-content.innerHTML = renderGame(nim);
+// function to track the input from the take button
+buttonTake.addEventListener('click', function(){
+    subtractPieces(inputValue);
+    if (playerOne) {
+        alert("Player 2's Turn!");
+        playerOne = false;
+    } else {
+        alert("Player 1's Turn!");
+        playerOne = true;
+    }
+});
+
+// Create a while loop to end the game when totalPieces has reached 1
+while (totalPieces > 1) {
+    content.innerHTML = renderGame(nim);
+}
+// var nim = "Replace this with your own abstraction of Nim"
+
+// content.innerHTML = renderGame(nim);
 
 
 
@@ -38,7 +78,7 @@ function renderGame(game) {
                     <option value="2">2</option>
                     <option value="3">3</option>
                 </select>
-                <button class="btn btn-primary">Take</button>
+                <button class="btn btn-primary" id="take">Take</button>
             </div>
         </div>
     `
